a5c69a153c8e4ef489e7a3608a46cc5051051d5e
Terry <terry.otieno@student.moringaschool.com>
2025-03-07 09:04:16 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import React, { useState } from 'react';
 import Home from './pages/home';
-import PlaylistPage from './pages/PlaylistPage'; // Import the PlaylistPage component
-import './App.css'; // Import your CSS file for styling
+import PlaylistPage from './pages/PlaylistPage'; 
+import './App.css';
 import Navbar from './components/Navbar';
 
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(0);
-  const itemsPerPage = 10; // Adjust based on your needs
-  const data = Array.from({ length: 100 }, (_, i) => `Item ${i + 1}`); // Example data
+  const itemsPerPage = 10;
+  const data = Array.from({ length: 100 }, (_, i) => `Item ${i + 1}`); 
 
   const pageCount = Math.ceil(data.length / itemsPerPage);
 
